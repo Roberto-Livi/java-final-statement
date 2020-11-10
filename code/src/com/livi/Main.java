15ac52646395705ec974171d6b7c3aaa577a8a1b
Roberto-Livi <robertomlivi@outlook.com>
2020-11-09 21:02:09 -0500

    testing the methods from the Password class in the Main method

--- code/src/com/livi/Main.java
+++ code/src/com/livi/Main.java
@@ -6,6 +6,20 @@ public class Main {
         SomeClass one = new SomeClass("one");
         SomeClass two = new SomeClass("two");
         SomeClass three = new SomeClass("three");
+
+        System.out.println(one.getInstanceNumber());
+        System.out.println(two.getInstanceNumber());
+        System.out.println(three.getInstanceNumber());
+
+        System.out.println(Math.PI);
+
+        int pw = 52923994;
+        Password password = new Password(pw);
+        password.storePassword();
+
+        password.letMeIn(1);
+        password.letMeIn(232422);
+        password.letMeIn(52923994);
     }
 
 }
