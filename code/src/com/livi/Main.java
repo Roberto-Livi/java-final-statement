36399d894766964ad60d8145c03897ac80ca494e
Roberto-Livi <robertomlivi@outlook.com>
2020-11-09 20:42:38 -0500

    demonstrating how the final statement works in the Main method by creating new instances

--- code/src/com/livi/Main.java
+++ code/src/com/livi/Main.java
@@ -3,7 +3,9 @@ package com.livi;
 public class Main {
 
     public static void main(String[] args) {
-
+        SomeClass one = new SomeClass("one");
+        SomeClass two = new SomeClass("two");
+        SomeClass three = new SomeClass("three");
     }
 
 }
